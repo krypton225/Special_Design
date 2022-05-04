6675dfab31131d43d01aef360b8e6e973d903ff5
krypton225 <k.mohamed.11.98@gmail.com>
2022-05-04 16:55:50 +0200

    ADD landing page with responsive toggle menu

---
+++ js/header-script.js
@@ -0,0 +1,57 @@
+/**
+ * * Making the background of the header in the landing page changing after a seconds automatically!
+ **/
+
+let landing = document.getElementById("landing");
+let landingIntroTitle = document.getElementsByClassName(
+  "landing__intro__title"
+)[0];
+
+const myImages = [
+  {
+    imgSrc: "background-1.jpg",
+    landingTitle: "hello there, man!",
+  },
+  {
+    imgSrc: "background-2.jpg",
+    landingTitle: "you are welcome!",
+  },
+  {
+    imgSrc: "background-3.jpg",
+    landingTitle: "you are welcome, second!",
+  },
+  {
+    imgSrc: "background-4.png",
+    landingTitle: "you are welcome, third!",
+  },
+  {
+    imgSrc: "background-5.jpg",
+    landingTitle: "you are welcome, fourth!",
+  },
+];
+
+const getRandomNumber = (index) => {
+  return Math.floor(Math.random() * index);
+};
+
+const counterOfImagesInLandingPage = () => {
+  setInterval(() => {
+    let myRandomNumber = getRandomNumber(myImages.length);
+
+    landing.style.backgroundImage = `url("../imgs/backgrounds-landing/${myImages[myRandomNumber].imgSrc}")`;
+    landingIntroTitle.textContent = myImages[myRandomNumber].landingTitle;
+  }, 3000);
+};
+
+counterOfImagesInLandingPage();
+
+/**
+ * * This is for opening the toggle menu of the nav bar.
+ */
+
+let landingBarsIcon = document.getElementById("barsIcon");
+let navBarMenu = document.getElementById("navBarMenu");
+
+landingBarsIcon.addEventListener("click", () => {
+  navBarMenu.classList.toggle("toggle-navbar-menu");
+});
